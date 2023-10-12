5a1d3112107173447160afe4210a4f3a97188866
= <=>
2023-10-12 16:01:16 -0300

    UserEmtity - getters methods unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,21 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  get createAt() {
+    return this.props.createdAt
+  }
+
 }
