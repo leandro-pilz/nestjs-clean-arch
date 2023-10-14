ae944a8f94c843f2cbc55ad83cb7a230e139bb8c
= <=>
2023-10-14 12:52:33 -0300

    Adding abstract class ClassValidatorFields

--- src/shared/infrastructure/env-config/domain/entities/validators/class-valitador-fields.ts
+++ src/shared/infrastructure/env-config/domain/entities/validators/class-valitador-fields.ts
@@ -1,10 +1,21 @@
+import { validateSync } from "class-validator";
 import { FieldsErrors, ValidatorFiedlsInterface } from "./validator-fields.interface";
+import { da } from "@faker-js/faker";
 
 export abstract class ClassalidatorFields<PropsValidated> implements ValidatorFiedlsInterface<PropsValidated> {
   errors: FieldsErrors = null;
   validatedData: PropsValidated = null;
   validate(data: any): boolean {
+    const errors = validateSync(data)
+    if(errors.length){
+      this.errors = {}
+      for(const error of errors){
+        this.errors[error.property] = Object.values(error.constraints)
+      }
+    } else {
+      this.validatedData = data
+    }
 
-
+    return !errors.length
   }
 }
