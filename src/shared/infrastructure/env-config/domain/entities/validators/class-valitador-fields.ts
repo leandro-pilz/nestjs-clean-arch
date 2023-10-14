54a30451848a5fcb0cd4ca39aa683f878cc62107
= <=>
2023-10-14 12:53:27 -0300

    Adding abstract class ClassValidatorFields

--- src/shared/infrastructure/env-config/domain/entities/validators/class-valitador-fields.ts
+++ src/shared/infrastructure/env-config/domain/entities/validators/class-valitador-fields.ts
@@ -1,15 +1,14 @@
 import { validateSync } from "class-validator";
 import { FieldsErrors, ValidatorFiedlsInterface } from "./validator-fields.interface";
-import { da } from "@faker-js/faker";
 
 export abstract class ClassalidatorFields<PropsValidated> implements ValidatorFiedlsInterface<PropsValidated> {
   errors: FieldsErrors = null;
   validatedData: PropsValidated = null;
   validate(data: any): boolean {
     const errors = validateSync(data)
-    if(errors.length){
+    if (errors.length) {
       this.errors = {}
-      for(const error of errors){
+      for (const error of errors) {
         this.errors[error.property] = Object.values(error.constraints)
       }
     } else {
